3863d48f8239de58f09d9c5b693876707b309dfc
LDBoyden <lewisboyden@gmail.com>
2017-01-12 19:14:19 +0000

    OpenCV prototype - Camera now operational.

--- CVM_AR/app/src/main/java/com/example/developer/cvm_ar/MainActivity.java
+++ CVM_AR/app/src/main/java/com/example/developer/cvm_ar/MainActivity.java
@@ -1,15 +1,32 @@
 package com.example.developer.cvm_ar;
 
+import android.nfc.Tag;
+import android.opengl.Visibility;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.SurfaceView;
+import android.widget.Switch;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.JavaCameraView;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+
+public class MainActivity extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2 {
 
     // Used to load the 'native-lib' library on application startup.
     static {
         System.loadLibrary("native-lib");
     }
+
+    private static String TAG = "MainActivity";
+    JavaCameraView camStream; //object of the surface view containing the camera feed "vidfeed"
+    Mat mRgba; //global variables are horrific
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,10 +34,59 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // Example of a call to a native method
-        TextView tv = (TextView) findViewById(R.id.sample_text);
+        /*  TextView tv = (TextView) findViewById(R.id.sample_text);
         tv.setText(stringFromJNI());
         TextView Suc = (TextView) findViewById(R.id.successText);
         Suc.setText("TestCase: OpenCV Install Success");
+        */
+
+        camStream = (JavaCameraView) findViewById(R.id.vidFeed); //assigning the surface view to the camera object
+        camStream.setVisibility(SurfaceView.VISIBLE);
+        camStream.setCvCameraViewListener(this);
+    }
+
+    BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
+        @Override
+        public void onManagerConnected(int status) {
+            switch(status){
+                case BaseLoaderCallback.SUCCESS:{
+                    camStream.enableView();
+                    break;
+                }
+                default: {
+                    super.onManagerConnected(status);
+                    break;
+                }
+            }
+        }
+    };
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        if (camStream != null){
+            camStream.disableView();
+        }
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        if (camStream != null){
+            camStream.disableView();
+        }
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        if (OpenCVLoader.initDebug()){
+            Log.i(TAG, "OpenCV Loaded Successfully");
+            mLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
+        }else{
+            Log.i(TAG, "OpenCV Not Loaded");
+            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_2_0,this, mLoaderCallback);
+        }
     }
 
     /**
@@ -28,4 +94,20 @@ public class MainActivity extends AppCompatActivity {
      * which is packaged with this application.
      */
     public native String stringFromJNI();
+
+    @Override
+    public void onCameraViewStarted(int width, int height) {
+        mRgba = new Mat(height,width, CvType.CV_8SC4);
+    }
+
+    @Override
+    public void onCameraViewStopped() {
+        mRgba.release();
+    }
+
+    @Override
+    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+        mRgba = inputFrame.rgba();
+        return mRgba;
+    }
 }
