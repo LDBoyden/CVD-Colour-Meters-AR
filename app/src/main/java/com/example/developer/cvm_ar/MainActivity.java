10488b744167e6a2d0ea6bc7062de3431c03c0f7
LDBoyden <lewisboyden@gmail.com>
2017-04-12 02:17:54 +0100

    Meter class refactored now all image processing done in its own class. To Do: Make DistanceGovernance class work on its own class.

--- app/src/main/java/com/example/developer/cvm_ar/MainActivity.java
+++ app/src/main/java/com/example/developer/cvm_ar/MainActivity.java
@@ -6,14 +6,20 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.GestureDetector;
 import android.view.SurfaceView;
 
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraBridgeViewBase;
 import org.opencv.android.JavaCameraView;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.core.CvType;
 import org.opencv.core.Mat;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2 {
 
     // Used to load the 'native-lib' library on application startup.
     static {
@@ -40,7 +46,7 @@ public class MainActivity extends AppCompatActivity {
 
         camStream = (JavaCameraView) findViewById(R.id.vidFeed); //assigning the surface view to the camera object
         camStream.setVisibility(SurfaceView.VISIBLE);
-        //camStream.setCvCameraViewListener(this);
+        camStream.setCvCameraViewListener(this);
 
         //mGestDet = new GestureDetector(this);
 
@@ -51,4 +57,78 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+    BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
+        @Override
+        public void onManagerConnected(int status) {
+            switch (status) {
+                case BaseLoaderCallback.SUCCESS: {
+                    camStream.enableView();
+                    break;
+                }
+                default: {
+                    super.onManagerConnected(status);
+                    break;
+                }
+            }
+        }
+    };
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (camStream != null) {
+            camStream.disableView();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (camStream != null) {
+            camStream.disableView();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (OpenCVLoader.initDebug()) {
+            Log.i(TAG, "OpenCV Loaded Successfully");
+            mLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
+        } else {
+            Log.i(TAG, "OpenCV Not Loaded");
+            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_2_0, this, mLoaderCallback);
+        }
+    }
+
+    @Override
+    public void onCameraViewStarted(int width, int height) {
+        mRgba = new Mat(height, width, CvType.CV_8UC4); // defines matrix as being the size of the screen with colour channels as 4
+    }
+
+    @Override
+    public void onCameraViewStopped() {
+        mRgba.release();
+    }
+
+    @Override
+    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+        mRgba = inputFrame.rgba(); //renders frames from video in colour {R,G,B,A} each pixel?.#
+
+        int numCols = mColNum;
+        int numRows = mRowNum;
+
+        int rectWidth = mRgba.width()/numCols;
+        int rectHeight = mRgba.height()/numRows;
+        Meter meter = new Meter();
+
+        for(int i = 0; i < numCols; i++) {
+            int topLeftX = rectWidth * i;
+            for (int j = 0; j < numRows; j++) {
+                int topLeftY = rectHeight * j;
+                meter.DrawDetectionSq(mRgba, topLeftX, topLeftY, rectWidth, rectHeight);
+            }
+        }
+        return mRgba;  // return value should be output value
+    }
 }
--- app/src/main/java/com/example/developer/cvm_ar/Meter.java
+++ app/src/main/java/com/example/developer/cvm_ar/Meter.java
@@ -1,13 +1,5 @@
 package com.example.developer.cvm_ar;
 
-import android.app.Activity;
-import android.util.Log;
-
-import org.opencv.android.BaseLoaderCallback;
-import org.opencv.android.CameraBridgeViewBase;
-import org.opencv.android.LoaderCallbackInterface;
-import org.opencv.android.OpenCVLoader;
-import org.opencv.core.CvType;
 import org.opencv.core.Mat;
 import org.opencv.core.Point;
 import org.opencv.core.Rect;
@@ -15,86 +7,11 @@ import org.opencv.core.Scalar;
 import org.opencv.core.Size;
 import org.opencv.imgproc.Imgproc;
 
-public class Meter extends MainActivity implements CameraBridgeViewBase.CvCameraViewListener2 {
+public class Meter {
 
     private static String TAG = "MeterClass";
 
-
-    BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
-        @Override
-        public void onManagerConnected(int status) {
-            switch (status) {
-                case BaseLoaderCallback.SUCCESS: {
-                    camStream.enableView();
-                    break;
-                }
-                default: {
-                    super.onManagerConnected(status);
-                    break;
-                }
-            }
-        }
-    };
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        if (camStream != null) {
-            camStream.disableView();
-        }
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        if (camStream != null) {
-            camStream.disableView();
-        }
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        if (OpenCVLoader.initDebug()) {
-            Log.i(TAG, "OpenCV Loaded Successfully");
-            mLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
-        } else {
-            Log.i(TAG, "OpenCV Not Loaded");
-            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_2_0, this, mLoaderCallback);
-        }
-    }
-
-    @Override
-    public void onCameraViewStarted(int width, int height) {
-        mRgba = new Mat(height, width, CvType.CV_8UC4); // defines matrix as being the size of the screen with colour channels as 4
-    }
-
-    @Override
-    public void onCameraViewStopped() {
-        mRgba.release();
-    }
-
-    @Override
-    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
-        mRgba = inputFrame.rgba(); //renders frames from video in colour {R,G,B,A} each pixel?.
-
-        int numCols = mColNum;
-        int numRows = mRowNum;
-
-        int rectWidth = mRgba.width()/numCols;
-        int rectHeight = mRgba.height()/numRows;
-
-        for(int i = 0; i < numCols; i++) {
-            int topLeftX = rectWidth * i;
-            for (int j = 0; j < numRows; j++) {
-                int topLeftY = rectHeight * j;
-                DrawDetectionSq(topLeftX, topLeftY, rectWidth, rectHeight);
-            }
-        }
-        return mRgba;  // return value should be output value
-    }
-
-    public void DrawDetectionSq(int topLeftX, int topLeftY, int rectWidth, int rectHeight) {
+    public void DrawDetectionSq(Mat mRgba ,int topLeftX, int topLeftY, int rectWidth, int rectHeight) {
 
         // find the ellipse coordinates and draw the ellipse
         int centreX = topLeftX + (rectWidth/2);
@@ -104,7 +21,7 @@ public class Meter extends MainActivity implements CameraBridgeViewBase.CvCamera
 
 
         // Line Calculations By David Flatla.
-        Scalar mColHSV = GetHSVColourAt(centreX, centreY);
+        Scalar mColHSV = GetHSVColourAt(mRgba,centreX, centreY);
         double rads = (mColHSV.val[0] / 128.0) * Math.PI;   // 127.5
         double satRadius = ellipseRadius * (mColHSV.val[1]/256.0);
         int x = (int) Math.round(satRadius * Math.cos(rads));
@@ -120,7 +37,7 @@ public class Meter extends MainActivity implements CameraBridgeViewBase.CvCamera
 //        Imgproc.putText(mRgba, hue, ellipseCentre, 1, 2.0, new Scalar(200,100,100), 3);
     }
 
-    public Scalar GetHSVColourAt(int centreX, int centreY) {
+    public Scalar GetHSVColourAt(Mat mRgba, int centreX, int centreY) {
         // define the detection square (1x1 centred at above coordinates)
         Rect DetSQ = new Rect();
         DetSQ.x = centreX;
